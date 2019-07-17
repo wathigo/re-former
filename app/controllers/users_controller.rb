0869279159fc97bd7155b94af52eae7d4a54c975
shjang7 <lucy.sh.jang@gmail.com>
2019-07-18 00:57:52 +0900

    Finish html-form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,15 +26,22 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+    # @user = User.new(user_params)
+    #
+    # respond_to do |format|
+    #   if @user.save
+    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
+    #     format.json { render :show, status: :created, location: @user }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @user.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /users/1
@@ -69,6 +76,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
+      # username: params[:username], email: params[:email], password: params[:password]
       params.require(:user).permit(:username, :email, :password)
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  private
+
+  # def html_options_for_form(url_for_options, options)
+  #   html_options = super(url_for_options, options)
+  #   html_options["accept-charset"] = "ISO-8859-1"
+  #   html_options
+  # end
+  #
+  # def utf8_enforcer_tag
+  #   "".html_safe
+  # end
 end
