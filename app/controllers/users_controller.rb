4b6ae9e6959483cc2ec0ea2610c949bf7599b490
wathigo <wathigosimon@gmail.com>
2019-07-18 06:09:12 -0400

    Use form_tag helper method to create a form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,8 +24,8 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
+    # @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     if @user.save
       redirect_to new_user_path
     else
